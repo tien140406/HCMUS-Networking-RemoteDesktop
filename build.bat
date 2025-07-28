fc65fa7efb9b6a653a22d11cbafbdf11a22ae7ba
htham-eituc <ngohuynhtham@gmail.com>
2025-07-28 07:40:06 +0700

    [Edit] cmake

--- build.bat
+++ build.bat
@@ -1,36 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM === Setup MSYS2 MinGW64 environment ===
-echo [INFO] Setting up MSYS2 MinGW64 environment...
-set "PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%"
-set "PKG_CONFIG_PATH=C:\msys64\mingw64\lib\pkgconfig"
-set "CC=C:\msys64\mingw64\bin\gcc.exe"
-set "CXX=C:\msys64\mingw64\bin\g++.exe"
+echo [INFO] Simple MSYS2 build script
 
-REM === Remove old build ===
+REM === Basic MSYS2 setup ===
+set "MSYS_ROOT=D:\msys64"
+set "PATH=%MSYS_ROOT%\mingw64\bin;%MSYS_ROOT%\usr\bin;%PATH%"
+
+REM === Clean build ===
 if exist build (
-    echo [INFO] Removing old 'build' directory...
+    echo [INFO] Removing old build...
     rmdir /s /q build
 )
-
 mkdir build
 cd build
 
-REM === Configure with CMake ===
-echo [INFO] Configuring with CMake...
-cmake .. -G "MinGW Makefiles" ^
-  -DPKG_CONFIG_EXECUTABLE="C:/msys64/usr/bin/pkg-config.exe"
+REM === Simple CMake call ===
+echo [INFO] Running CMake...
+cmake .. -G "MinGW Makefiles"
 
 if errorlevel 1 (
-    echo [ERROR] CMake configuration failed!
+    echo [ERROR] CMake failed!
     pause
     exit /b 1
 )
 
 REM === Build ===
-echo ==================================================
-echo [INFO] Building project...
+echo [INFO] Building...
 cmake --build .
 
 if errorlevel 1 (
@@ -39,25 +35,10 @@ if errorlevel 1 (
     exit /b 1
 )
 
-REM === Copy ALL DLLs from MSYS2 MinGW64 ===
-echo ==================================================
-echo [INFO] Copying all DLLs from C:\msys64\mingw64\bin ...
-for %%f in (C:\msys64\mingw64\bin\*.dll) do (
-    if exist "%%f" (
-        copy "%%f" . >nul
-        echo [OK] Copied %%~nxf
-    )
-)
+@REM REM === Copy DLLs ===
+@REM echo [INFO] Copying DLLs...
+@REM copy "%MSYS_ROOT%\mingw64\bin\*.dll" . >nul 2>&1
 
-REM === Final info ===
-echo ==================================================
-echo [INFO] Listing copied DLLs:
-dir /b *.dll
-
-REM === Auto-set Bash PATH ===
-set PATH=%CD%;%PATH%
-
-echo ==================================================
-echo [INFO] Build Complete!
+echo [INFO] Build complete!
+echo [INFO] To run: make sure %MSYS_ROOT%\mingw64\bin is in your Windows PATH
 pause
-endlocal

